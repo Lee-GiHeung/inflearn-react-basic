3b7026e29162dbe1a6f6a2da1a61be47e6d9e541
Lee-GiHeung <dlrlgmd25@gmail.com>
2024-07-18 14:57:23 +0900

    State로 사용자 입력 관리하기1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
-import Bulb from './components/Bulb';
-import Counter from './components/Counter';
+import Register from './components/Register';
 
 // 부모
 function App() {
   return (
     <>
-      <Bulb />
-      <Counter />
+      <Register />
     </>
   );
 }
